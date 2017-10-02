7b61d570aaec6c6d008cd262da4a9c4bd2024e59
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-10-02 17:28:59 +0530

    Variables (018).

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -5,5 +5,15 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+
+    int myFirstNumber = (10 + 5) + (2 * 10);
+    int mySecondNumber = 12;
+    int myThirdNumber = myFirstNumber * 2;
+
+    int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+    int myFinalNumber = 1000 - myTotal;
+
+    System.out.println(myFinalNumber);
   }
 }
