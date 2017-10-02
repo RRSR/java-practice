9bed4f27e7c15871d6e86b05bf2d3115497ce4c4
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-10-02 22:27:37 +0530

    added my name in hindi.

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -13,5 +13,16 @@ public class Main {
     //Challenge
     char charValue = '\u00AE';
     System.out.println("The Registered symbol looks like : " + charValue);
+
+    //Writing my name in hindi
+    char char1= '\u0930';
+    char char2= '\u093e';
+    char char3= '\u091c';
+    char char4= '\u0947';
+    char char5= '\u0936';
+    char char6= '\u094d';
+    char char7= '\u0935';
+    System.out.println("My name is : "+char1+char2+char3+" "+char1+char2+char3+char4+char5+char6+char7+char1);
+
   }
 }
