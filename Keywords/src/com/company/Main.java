e0ff6b4275850773cf93224d68ba6769bd891042
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-10-07 11:09:03 +0530

    Keywords and expressions (026).

---
+++ Keywords/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+  public static void main(String[] args) {
+    //In the following code, the parts which are expressions are given in comments at the end.
+
+    int score = 100;    // score=100
+    if(score > 99){     // score >99
+      System.out.println("You got the high score!");    //"You got the high score!"
+      score = 0;    //score = 0
+    }
+  }
+}
