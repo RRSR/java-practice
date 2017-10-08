9d1c64c0ee5a333bf1b6768fb3f38921ffa16e04
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-10-08 21:10:57 +0530

    high score position challenge (030).

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -18,15 +18,37 @@ public class Main {
     result = calculateScore(gameOver, score, levelCompleted, bonus);
     System.out.println("Your first final score is : " + result);
 
+    displayHighScorePosition("Raj", calculateHighScorePosition(1000));
+    displayHighScorePosition("Hari", calculateHighScorePosition(900));
+    displayHighScorePosition("Jai", calculateHighScorePosition(100));
+    displayHighScorePosition("Mohan", calculateHighScorePosition(50));
+
   }
 
-  static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+  private static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
     if (gameOver) {
       int finalScore = score + (levelCompleted * bonus);
       finalScore += 2000;
       return finalScore;
     }
     return 0;
+  }
+
+  private static void displayHighScorePosition(String name, int position) {
+    System.out
+        .println(name + " managed to get into position " + position + " on the high score table.");
+  }
+
+  private static int calculateHighScorePosition(int score) {
+    if (score >= 1000) {
+      return 1;
+    } else if (score >= 500) {
+      return 2;
+    } else if (score >= 100) {
+      return 3;
+    } else {
+      return 4;
+    }
   }
 
 }
