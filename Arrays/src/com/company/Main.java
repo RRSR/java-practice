846537f9ca326925c9099ab9dc4df46ea4755f5c
Raj Rathore <Raj.Rathore@tatacommunications.com>
2017-11-19 16:14:18 +0530

    Arrays Challenge Exercise (055).

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -11,16 +12,29 @@ public class Main {
     int number = scanner.nextInt();
     int[] integerArray = getIntegers(number);
     printArray(integerArray);
-    System.out.println(getAverage(integerArray));
+    System.out.println(Arrays.toString(sortArray(integerArray)));
   }
 
-  private static double getAverage(int[] array) {
+  private static int[] sortArray(int[] array) {
+    for(int i=0;i<array.length-1;i++){
+      for(int j=i+1;j<array.length;j++){
+        if(array[j] > array[i]){
+          array[i] = array[j] + array[i];
+          array[j] = array[i] - array[j];
+          array[i] = array[i] - array[j];
+        }
+      }
+    }
+    return array;
+  }
+
+  /*private static double getAverage(int[] array) {
     double sum = 0;
     for (int value : array) {
       sum += value;
     }
     return sum / (double) array.length;
-  }
+  }*/
 
   private static int[] getIntegers(int number) {
     System.out.println("Enter " + number + " integer values");
